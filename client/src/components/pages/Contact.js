a14a4ec1ac2c40f583f79737c267e7b59fa909a4
BRASS <mr.brn.lewis@outlook.com>
2021-08-29 01:27:15 +0800

    contact page up

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -5,8 +5,44 @@ import Header from '../Header'
 function Contact() {
     return (
         <div>
-        <Header></Header>
-        
+            <Header></Header>
+<main className="showcase">
+    <div className="main-content">
+
+<section>
+    
+
+<div className="project-intro" data-component->
+
+
+<div className="contact">
+
+<h4> CONTACT DETAILS </h4>
+
+
+<p id="contactDeets">Name: Brendan Lewis   <br />Location: Perth, WA<br/>Email: <a href="mailto:lewis_brendan@outlook.com"> lewis_brendan@outlook.com </a></p>
+
+<p> </p>
+
+<h5> <a href="./BLresume.pdf" id="resume"> Résumé </a> </h5></div>
+
+
+
+<div className="socials">
+
+<a href="https://github.com/operationBrass" className="fa fa-github"></a>
+
+<a href="https://www.linkedin.com/in/brendanlewis-brass/" className="fa fa-linkedin"></a>
+
+<a href="#" className="fa fa-youtube"></a>
+
+<a href="#" className="fa fa-facebook"></a>
+
+</div>
+</div>
+</section>
+</div>
+</main>
         </div>
     )
 }
