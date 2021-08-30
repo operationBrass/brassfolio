d7ef58fe360987c68159538e8f3d436efd089d32
BRASS <mr.brn.lewis@outlook.com>
2021-08-30 19:56:37 +0800

    finished projects

--- client/src/components/pages/Project.js
+++ client/src/components/pages/Project.js
@@ -20,24 +20,25 @@ function Project() {
                   <Reveal animated='move up'>
                 <Reveal.Content hidden>
                     <Card fluid>
+       <Image className="projectImages" src='./images/passwordGen.png'/>
                     <Card.Content  id="passgen">
-            <Image src='./images/passwordGen.png' fluid />
-          
+            
+                    
               <Card.Header>Password Generator</Card.Header>
               <Card.Meta>
-                <span className='date' >City Weather Forecast</span>
+                <span className='date' >Create unique passwords</span>
               </Card.Meta>
                 <Card.Description>
                   The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
-                  accepting user input, validating user input and returning password based on users specific criteria. 
-                  Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+                  accepting user input, validating user input and returning password based on users specific criteria.
                 </Card.Description>
             </Card.Content>
           <Card.Content extra>
-            <a>
-            <Icon name='user' />
-            Contact me
-            </a>
+            <a href="https://github.com/operationBrass/homework-assignment-3">
+            <Icon name='github' size="huge" />
+     
+                        </a>
+                        <a href="/contact"> <Icon name='question circle outline' size="huge" />     </a>
                   </Card.Content>
                 </Card>
                   </Reveal.Content>
@@ -50,25 +51,25 @@ function Project() {
 
               <Reveal animated='move up'>
         <Reveal.Content hidden>
-            <Card fluid>
-            <Card.Content id="weather">
-      <Image src='./images/weatherForecast.png' fluid />
+                    <Card fluid>
+                    <Image src='./images/quiz.png' className="projectImages" />
+            <Card.Content id="quiz" >
+     
       
-        <Card.Header>Password Generator</Card.Header>
+        <Card.Header>Code Quiz</Card.Header>
         <Card.Meta>
-          <span className='date'>Vanilla Javascript</span>
+          <span className='date'>Test your JS knowledge</span>
         </Card.Meta>
           <Card.Description>
-            The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
-            accepting user input, validating user input and returning password based on users specific criteria. 
-            Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+          Test your Javascript knowledge with this 5 question Quiz. Utilising bootstrap and jQuery to manage the animated 'carousel' and overall styling, consideration for mobile users and make sure to add your score to the leaderboard!
           </Card.Description>
       </Card.Content>
     <Card.Content extra>
-      <a>
-      <Icon name='user' />
-      Contact me
-      </a>
+      <a href="https://github.com/operationBrass/homework-assignment-4">
+      <Icon textAlign="center" name='github' size="huge" />
+            
+                        </a>
+                        <a href="/contact"> <Icon name='question circle outline' size="huge" />     </a>
             </Card.Content>
           </Card>
           </Reveal.Content>
@@ -90,25 +91,27 @@ function Project() {
               <Grid.Column>
               <Reveal animated='move up'>
               <Reveal.Content hidden>
-                  <Card fluid>
+                    <Card fluid>
+                    <Image src='./images/weatherForecast.png'className="projectImages"  />
                   <Card.Content id="weather">
-            <Image src='./images/weatherForecast.png' fluid />
+    
             
-              <Card.Header>Password Generator</Card.Header>
+              <Card.Header>Weather Forecast</Card.Header>
               <Card.Meta>
-                <span className='date'>Vanilla Javascript</span>
+                <span className='date'>Search local weather reports</span>
               </Card.Meta>
                 <Card.Description>
-                  The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
-                  accepting user input, validating user input and returning password based on users specific criteria. 
-                  Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+                          The Weather Dashboard uses API service to return to you a weather forecast with a 5 day outlook. You can search for any valid city and add to your history for easy refresh!
+                          During this assignment i was introduced to API calls and Javascript Promises
+
+
                 </Card.Description>
             </Card.Content>
           <Card.Content extra>
-            <a>
-            <Icon name='user' />
-            Contact me
-            </a>
+            <a href="https://github.com/operationBrass/homework-assignment-6">
+             <Icon name='github' size="huge" />
+                        </a>
+                        <a href="/contact"> <Icon name='question circle outline' size="huge" />     </a>
                   </Card.Content>
                 </Card>
                 </Reveal.Content>
@@ -123,13 +126,14 @@ function Project() {
 
               <Reveal animated='move up'>
           <Reveal.Content hidden>
-              <Card fluid>
-              <Card.Content  id="passgen">
-      <Image src='./images/techBlog.png' fluid />
+                    <Card fluid>
+                    <Image src='./images/techBlog.png' className="projectImages" />
+              <Card.Content  id="techblog">
+
     
-        <Card.Header>Password Generator</Card.Header>
+        <Card.Header>Tech Blog</Card.Header>
         <Card.Meta>
-          <span className='date' >City Weather Forecast</span>
+          <span className='date' >Signup and Blog</span>
         </Card.Meta>
           <Card.Description>
             The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
@@ -138,10 +142,10 @@ function Project() {
           </Card.Description>
       </Card.Content>
     <Card.Content extra>
-      <a>
-      <Icon name='user' />
-      Contact me
-      </a>
+      <a href="https://github.com/operationBrass/homework-assignment-14">
+      <Icon name='github' size="huge" />
+                        </a>
+                        <a href="/contact"> <Icon name='question circle outline' size="huge" />     </a>
             </Card.Content>
           </Card>
             </Reveal.Content>
@@ -160,25 +164,25 @@ function Project() {
       <Grid.Column>
       <Reveal animated='move up'>
       <Reveal.Content hidden>
-          <Card fluid>
-          <Card.Content id="weather">
-    <Image src='./images/weatherForecast.png' fluid />
+                    <Card fluid>
+                    <Image src='./images/teamgen.png' className="projectImages"/>
+          <Card.Content id="teamprof">
+  
     
-      <Card.Header>Password Generator</Card.Header>
+      <Card.Header>Team Profile</Card.Header>
       <Card.Meta>
-        <span className='date'>Vanilla Javascript</span>
+        <span className='date'>Manage your teams</span>
       </Card.Meta>
         <Card.Description>
-          The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
-          accepting user input, validating user input and returning password based on users specific criteria. 
-          Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+        CLI application takes in employee information and provides a HTML report of the team members. This project being a nice introduction to the Inqurier package in particular.
         </Card.Description>
     </Card.Content>
   <Card.Content extra>
-    <a>
-    <Icon name='user' />
-    Contact me
-    </a>
+    <a href="https://github.com/operationBrass/homework-assignment-10">
+    <Icon name='github' size="huge" />
+
+                        </a>
+                        <a href="/contact"> <Icon name='question circle outline' size="huge" />     </a>
           </Card.Content>
         </Card>
         </Reveal.Content>
@@ -193,25 +197,27 @@ function Project() {
 
       <Reveal animated='move up'>
   <Reveal.Content hidden>
-      <Card fluid>
-      <Card.Content  id="passgen">
-<Image src='./images/realmeal.PNG' fluid />
+                    <Card fluid>
+                    <Image src='./images/therealmealApp.PNG' className="projectImages" />
+      <Card.Content  id="realmeal">
 
-<Card.Header>Password Generator</Card.Header>
+
+<Card.Header>The Real Meal</Card.Header>
 <Card.Meta>
-  <span className='date' >City Weather Forecast</span>
+  <span className='date' >Real Meal Planning</span>
 </Card.Meta>
   <Card.Description>
-    The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
-    accepting user input, validating user input and returning password based on users specific criteria. 
-    Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+  The Real-Meal app was the first real-life project created with a group of aspiring web developers taking part in UWA coding bootcamp 2021. 
+                The project introduced a number
+                of new frameworks and techniques, including the use of APIs and CSS Framework Bulma. Particular emphasis placed on the planning phase of s/w dev, including story boards, git branching and press release presentations! 
+
   </Card.Description>
 </Card.Content>
 <Card.Content extra>
-<a>
-<Icon name='user' />
-Contact me
-</a>
+<a href="https://github.com/michaelshxw/The-Real-Meal-v2">
+<Icon name='github' size="huge" />
+                        </a>
+                        <a href="/contact"> <Icon name='question circle outline' size="huge" />     </a>
     </Card.Content>
   </Card>
     </Reveal.Content>
