2369c50ad194f61e9971a35231aa5fd4d574dfb8
BRASS <mr.brn.lewis@outlook.com>
2021-08-30 17:23:39 +0800

    built up project logos

--- client/src/components/pages/Project.js
+++ client/src/components/pages/Project.js
@@ -1,100 +1,236 @@
 import Heading from '../Heading'
-import { Card, Segment, Divider, Header, Image, Icon, Container } from 'semantic-ui-react'
+import { Card, Reveal, Divider, Segment, Image, Icon, Grid, Container } from 'semantic-ui-react'
 
 
 function Project() {
     return (
 <div>
-            <Heading></Heading>
+        <Heading>Project History </Heading>
+        <Divider hidden />
+     
+       
+        
+ 
+        <Container text>
+          
+        <Grid columns={2} >
+          <Grid.Row stretched>
+      
+            <Grid.Column>
+                  <Reveal animated='move up'>
+                <Reveal.Content hidden>
+                    <Card fluid>
+                    <Card.Content  id="passgen">
+            <Image src='./images/passwordGen.png' fluid />
+          
+              <Card.Header>Password Generator</Card.Header>
+              <Card.Meta>
+                <span className='date' >City Weather Forecast</span>
+              </Card.Meta>
+                <Card.Description>
+                  The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
+                  accepting user input, validating user input and returning password based on users specific criteria. 
+                  Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+                </Card.Description>
+            </Card.Content>
+          <Card.Content extra>
+            <a>
+            <Icon name='user' />
+            Contact me
+            </a>
+                  </Card.Content>
+                </Card>
+                  </Reveal.Content>
+                  <Reveal.Content visible>
+                      <Image className="setSize" src="./images/passgenLogo-2.PNG"/>
+                    </Reveal.Content>
+              </Reveal> </Grid.Column>
+            
+              <Grid.Column>
 
+              <Reveal animated='move up'>
+        <Reveal.Content hidden>
+            <Card fluid>
+            <Card.Content id="weather">
+      <Image src='./images/weatherForecast.png' fluid />
+      
+        <Card.Header>Password Generator</Card.Header>
+        <Card.Meta>
+          <span className='date'>Vanilla Javascript</span>
+        </Card.Meta>
+          <Card.Description>
+            The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
+            accepting user input, validating user input and returning password based on users specific criteria. 
+            Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+          </Card.Description>
+      </Card.Content>
+    <Card.Content extra>
+      <a>
+      <Icon name='user' />
+      Contact me
+      </a>
+            </Card.Content>
+          </Card>
+          </Reveal.Content>
+          <Reveal.Content visible>
+                <Image className="setSize" src="./images/quizlogo-1.PNG"/>
+              </Reveal.Content>
+        </Reveal>
 
-<Divider hidden />
-
-            <Container text>
+              
                 
-<Card >
-    <Image src='./images/passwordGen.png' wrapped ui={false} />
-    <Card.Content>
+                
+                
+          </Grid.Column>
+
+            </Grid.Row>
+
+            <Grid.Row stretched>
+      
+              <Grid.Column>
+              <Reveal animated='move up'>
+              <Reveal.Content hidden>
+                  <Card fluid>
+                  <Card.Content id="weather">
+            <Image src='./images/weatherForecast.png' fluid />
+            
+              <Card.Header>Password Generator</Card.Header>
+              <Card.Meta>
+                <span className='date'>Vanilla Javascript</span>
+              </Card.Meta>
+                <Card.Description>
+                  The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
+                  accepting user input, validating user input and returning password based on users specific criteria. 
+                  Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+                </Card.Description>
+            </Card.Content>
+          <Card.Content extra>
+            <a>
+            <Icon name='user' />
+            Contact me
+            </a>
+                  </Card.Content>
+                </Card>
+                </Reveal.Content>
+                <Reveal.Content visible>
+                      <Image className="setSize" src="./images/weatherLogo.PNG"/>
+                    </Reveal.Content>
+              </Reveal>
+              </Grid.Column>
+          
+      
+              <Grid.Column>
+
+              <Reveal animated='move up'>
+          <Reveal.Content hidden>
+              <Card fluid>
+              <Card.Content  id="passgen">
+      <Image src='./images/techBlog.png' fluid />
+    
+        <Card.Header>Password Generator</Card.Header>
+        <Card.Meta>
+          <span className='date' >City Weather Forecast</span>
+        </Card.Meta>
+          <Card.Description>
+            The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
+            accepting user input, validating user input and returning password based on users specific criteria. 
+            Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+          </Card.Description>
+      </Card.Content>
+    <Card.Content extra>
+      <a>
+      <Icon name='user' />
+      Contact me
+      </a>
+            </Card.Content>
+          </Card>
+            </Reveal.Content>
+            <Reveal.Content visible>
+                <Image className="setSize" src="./images/techblog-logo.PNG"/>
+              </Reveal.Content>
+        </Reveal>
+                
+
+        </Grid.Column>
+
+            </Grid.Row>
+            
+            <Grid.Row stretched>
+      
+      <Grid.Column>
+      <Reveal animated='move up'>
+      <Reveal.Content hidden>
+          <Card fluid>
+          <Card.Content id="weather">
+    <Image src='./images/weatherForecast.png' fluid />
+    
       <Card.Header>Password Generator</Card.Header>
       <Card.Meta>
         <span className='date'>Vanilla Javascript</span>
       </Card.Meta>
-      <Card.Description>
-      The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
-                   accepting user input, validating user input and returning password based on users specific criteria. 
-                    Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
-      </Card.Description>
+        <Card.Description>
+          The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
+          accepting user input, validating user input and returning password based on users specific criteria. 
+          Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+        </Card.Description>
     </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        Contact me
-      </a>
+  <Card.Content extra>
+    <a>
+    <Icon name='user' />
+    Contact me
+    </a>
+          </Card.Content>
+        </Card>
+        </Reveal.Content>
+        <Reveal.Content visible>
+              <Image className="setSize" src="./images/teamgen-logo.PNG"/>
+            </Reveal.Content>
+      </Reveal>
+      </Grid.Column>
+  
+
+      <Grid.Column>
+
+      <Reveal animated='move up'>
+  <Reveal.Content hidden>
+      <Card fluid>
+      <Card.Content  id="passgen">
+<Image src='./images/realmeal.PNG' fluid />
+
+<Card.Header>Password Generator</Card.Header>
+<Card.Meta>
+  <span className='date' >City Weather Forecast</span>
+</Card.Meta>
+  <Card.Description>
+    The Password Generator was one of my first appications using basic Javascript. The app generates a password between 8-128 characters with added selection crtiera. It helped to develop skills with 
+    accepting user input, validating user input and returning password based on users specific criteria. 
+    Interesting learnings from  producing a consistant result when all criteria selected and only 8 character password choosen.
+  </Card.Description>
+</Card.Content>
+<Card.Content extra>
+<a>
+<Icon name='user' />
+Contact me
+</a>
     </Card.Content>
   </Card>
+    </Reveal.Content>
+    <Reveal.Content visible>
+        <Image className="setSize" src="./images/realmealLogo.PNG"/>
+      </Reveal.Content>
+</Reveal>
+        
 
+</Grid.Column>
+
+</Grid.Row>
+            </Grid>
             </Container>
+      
+        
             
-            <Divider hidden />
-            
-            <Container text>
-  
-                <Card >
-                    <Image src='./images/weatherForecast.png' wrapped ui={false} />
-                    <Card.Content>
-                      <Card.Header>City Weather Search</Card.Header>
-                      <Card.Meta>
-                        <span className='date'>Dynamic Events</span>
-                      </Card.Meta>
-                      <Card.Description>
-                     
-                     The Weather Forecast app was the first introduction to using APIs. It taught the use of JS promises as well as dynamically managing content. The app will display a 5 day forecast to any valid City searched. 
-                    
-                   Search results are saved to local storage and retrieved on page load. In the future, the history will return refreshed weather reports! 
-                      </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                      <a>
-                        <Icon name='user' />
-                        Deployment URL: <a href="https://operationbrass.github.io/homework-assignment-6/"> Weather Forecast </a> <br/> GITHUB: <a href="https://github.com/operationBrass/homework-assignment-6"> Weather-Repo </a> 
-
-                      </a>
-                    </Card.Content>
-                  </Card>
-                
-                            </Container>
-
-
-            <Divider hidden />
-            
-            <Container text>
-  
-  <Card >
-      <Image src='./images/therealmealApp.PNG' wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>The Real Meal </Card.Header>
-        <Card.Meta>
-          <span className='date'>Collobrative Project</span>
-        </Card.Meta>
-        <Card.Description>
-       
- The Real-Meal app was the first real-life project created with a group of aspiring web developers taking part in UWA coding bootcamp 2021. 
-  The project introduced a number
-  of new frameworks and techniques, including the use of APIs and CSS Framework Bulma. Particular emphasis placed on the planning phase of s/w dev, including story boards, git branching and press release presentations! 
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name='user' />
-          Deployment URL: <a href="https://michaelshxw.github.io/The-Real-Meal-App/"> The-Real-Meal </a> <br/> GITHUB: <a href="https://github.com/michaelshxw/The-Real-Meal-App"> Project-Repo </a> 
-
-        </a>
-      </Card.Content>
-    </Card>
-  
-              </Container>
-                   
-</div >);
+          
+</div>);
 }
                                                 
 export default Project;
