7f45d82e169154adb754f3b22439ebfce41b17b8
BRASS <mr.brn.lewis@outlook.com>
2021-08-31 10:41:41 +0800

    home page mod and some bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Project from './components/pages/Project'
 import Layout from './components/Layout';
 import 'semantic-ui-css/semantic.min.css'
 import background from './images/explorersunset.jpg'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, BrowserRouter} from 'react-router-dom';
 
 
 function App() {
@@ -21,15 +21,14 @@ function App() {
         backgroundPosition:"bottom"
     }}
 >
-
+      <BrowserRouter basename="/brassfolio" />
       <Router>
         <Switch>
           <Layout>
       <Route exact path="/" component={Home}>
 
       </Route>
-      <Route exact path="/projects" component={Project}>
-        
+      <Route exact path="/projects" component={Project}>  
       </Route>
       <Route exact path="/contact" component={Contact}>
      
@@ -38,7 +37,8 @@ function App() {
           </Route>
           </Layout>
           </Switch>
-          </Router>
+      </Router>
+
       </div>
   );
 }
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,19 +1,39 @@
 import { Link } from 'react-router-dom'
+import { Header, Button, Icon,  Grid} from 'semantic-ui-react'
 
 function Home() {
-    return (<div>
-    <section className="showcase-intro">
-      <div className="showcase-title">
-        <Link to="/about"><h1>Brendan <br />Lewis</h1></Link>
-      </div>
-      <div className="showcase-enter">
-        <p>
+  return (<div style={{
+    Height: '100vh', marginTop:"8%"}}>
+<Grid textAlign="center" verticalAlign="middle" >
+        <Grid.Row>
+          <Grid.Column>
+        <Link to="/about"><Header as="h1" inverted color="olive">Brendan Lewis</Header></Link>
+          </Grid.Column>
+          <Grid.Column>
           <Link className="butt-styles" to="/projects">
-            BROWSE MY WORK
-          </Link>
-        </p>
-      </div>
-    </section>
-</div>);
+          <Header as="h1" inverted color="olive"> Web Developer </Header> 
+            </Link>
+          </Grid.Column>
+            
+      </Grid.Row>
+      
+      <Grid.Row>
+    <a href="http://wwww.facebook.com"> 
+    <Button color='facebook'>
+            <Icon name='facebook' /> Facebook
+            </Button>
+      </a>
+      <a href="http://www.twitter.com"><Button color='twitter'>
+      <Icon name='twitter' /> Twitter
+    </Button> </a>
+    <a href="http://www.linkedin.com"><Button color='linkedin'>
+      <Icon name='linkedin' /> LinkedIn
+    </Button> </a>
+    <a href="http://www.youtube.com"><Button color='youtube'>
+      <Icon name='youtube' /> YouTube
+          </Button> </a>
+        </Grid.Row>
+      </Grid>
+      </div>);
 }
 export default Home;
