aa6b1bca4b2c414ccfca81d05806d86a3505c330
BRASS <mr.brn.lewis@outlook.com>
2021-09-01 11:41:46 +0800

    updating about me styles

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { Card, Image, Icon, Container, Divider } from 'semantic-ui-react'
+import { Header,Card, Image, Icon, Container, Divider } from 'semantic-ui-react'
 import profilePic from '../../images/myprofilepic.jfif'
 
 const extra = (
@@ -14,13 +14,14 @@ const About = () => (
     <Container text >
         <Divider hidden />
         <Divider hidden />
-        <Divider hidden/>
+    <Divider hidden />
+    <Header as="h1" color="yellow" ><b>Brendan Lewis</b></Header>
         <Card>
             <Image src={profilePic}/>
             <Card.Content id="aboutme" fluid>IT Service Delivery professional and aspiring Web engineer. Having just completed coding bootcamp with UWA i have developed a strong foundation from which to expand on.
             Please feel free to browse my work or reach out on the contact me page. Thanks for visiting.
             </Card.Content>
-            <Card.Content extra>
+            <Card.Content extra textAlign="center">
             <a href="https://github.com/operationBrass/homework-assignment-3">
             <Icon name='github' size="huge" />
      
